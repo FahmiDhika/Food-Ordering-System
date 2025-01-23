d4fd31e520fa9611afd1ae2441f92e79751dca7f
FahmiDhika <fahmidhika810@gmail.com>
2025-01-23 13:47:47 +0700

    menambah line agar menu picture ter detect

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import MenuRoute from './routers/menuRoute'
 import userRoute from './routers/userRoute'
 import OrderRoute from './routers/orderRoute'
 import { PORT } from './global'
+import path from 'path'
 
 const app = express()
 app.use(cors())
@@ -11,6 +12,8 @@ app.use(cors())
 app.use(`/menu`, MenuRoute)
 app.use(`/user`, userRoute)
 app.use(`/order`, OrderRoute)
+
+app.use(express.static(path.join(__dirname, "..", "public")))
 
 app.listen(PORT, () => {
     console.log(`Server run on port http://localhost:${PORT}`)
